5e64f71e66ddb1c477e6f62375972596ec858127
toni-tar <toni.tarasenko.13@bk.ru>
2024-10-06 17:58:37 +0300

    BbBbBbBbBbBbBbB
    YES!!!This is new BbBbBbBbBbB project!!!Wow!!!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,15 +10,26 @@ public class PlayerController : MonoBehaviour
     private int rotationSpeed = 66;
     public Rigidbody Rigidbody;
     private int forse = 6;
+    private bool onIsland;
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * Time.deltaTime * speed * vertical);
         transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed * horizontal);
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)&& onIsland == true)
         {
             Rigidbody.AddForce(Vector3.up * forse, ForceMode.Impulse);
+            onIsland = false;
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Island")) {
+        
+        onIsland = true;
+        
         }
 
     }
